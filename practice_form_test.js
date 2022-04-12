a6d16da8b0bee60cc798ebc418b5f252569e5f39
Aleksandra Baryń <aleksanda.baryn@gmail.com>
2022-04-12 15:51:03 +0200

    Add date picker test

--- practice_form_test.js
+++ practice_form_test.js
@@ -7,9 +7,11 @@ Scenario('test something', ({ I }) => {
     I.fillField('#firstName', 'Jan');
     I.fillField('#lastName', 'Kowalski');   
     I.fillField('#userEmail', 'jan.kowalski@gmail.com');
-    I.fillField('#userNumber', '600700600');  
-    I.fillField('#lastName', 'Kowalski'); 
     I.checkOption('Male');
+    I.fillField('#userNumber', '600700600'); 
+    I.clearField('#dateOfBirthInput');
+    I.fillField('#dateOfBirthInput', '29 Feb 2000');
+    
     //I.see('Please select an item');
     //I.click('//*[@id="item-0"]/span');
     
